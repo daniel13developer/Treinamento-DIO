a3e3ab8af3135f9445a7230bd7821cabe88bc531
daniel13developer <daniel.developer13@hotmail.com>
2025-01-31 20:26:23 -0300

    correção do arquivo decrypter para funcionamento correto da descriptografia

--- projeto-ransomware/decrypter.py
+++ projeto-ransomware/decrypter.py
@@ -3,7 +3,7 @@ import pyaes # type: ignore
 
 ## abrir o arquivo criptografado
 file_name = "teste.txt.ransomwaretroll"
-file = open(file_name, "rb")
+file = open(file_name, 'rb')
 file_data = file.read()
 file.close()
 
@@ -13,14 +13,13 @@ aes = pyaes.AESModeOfOperationCTR(key)
 decrypt_data = aes.decrypt(file_data)
 
 ## remover o arquivo criptografado
-if os.path.exists(file_name):
-    os.remove(file_name)
-else:
-    print("Arquivo não encontrado para remoção.")
-os.remove(file_name)
+#os.remove(file_name)
 
 ## criar o arquivo descriptografado
 new_file = "teste.txt"
 new_file = open(f'{new_file}', "wb")
 new_file.write(decrypt_data)
 new_file.close()
+
+## remover o arquivo criptografado
+os.remove(file_name)
