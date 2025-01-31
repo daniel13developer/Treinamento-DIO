43b48b76bcfccbe583f3877e3e31ac37facfb978
daniel13developer <daniel.developer13@hotmail.com>
2025-01-31 19:56:28 -0300

    Criado arquivo em Python para criptografar o código teste.txt

---
+++ projeto-ransomware/decrypter.py
@@ -0,0 +1,26 @@
+import os
+import pyaes # type: ignore
+
+## abrir o arquivo criptografado
+file_name = "teste.txt.ransomwaretroll"
+file = open(file_name, "rb")
+file_data = file.read()
+file.close()
+
+## chave para descriptografia
+key = b"testeransomwares"
+aes = pyaes.AESModeOfOperationCTR(key)
+decrypt_data = aes.decrypt(file_data)
+
+## remover o arquivo criptografado
+if os.path.exists(file_name):
+    os.remove(file_name)
+else:
+    print("Arquivo não encontrado para remoção.")
+os.remove(file_name)
+
+## criar o arquivo descriptografado
+new_file = "teste.txt"
+new_file = open(f'{new_file}', "wb")
+new_file.write(decrypt_data)
+new_file.close()
